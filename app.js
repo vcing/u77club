2755e3e3950063e5a6db96bf03ed418ae8526d19
Vcing <2131287@qq.com>
2015-06-15 22:34:59 +0800

    基本布局 和 用户模块

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict';
-
-var express = require('express.oi'),
+var _       = require('lodash'),
+express     = require('express.oi'),
+mongoose    = require('mongoose'),
+MongoStore  = require('connect-mongo')(express.session),
+bodyParser  = require('body-parser'),
 settings    = require('./settings.json'),
 app         = express(),
-MongoStore  = require('connect-mongo')(express.session),
-mongoose    = require('mongoose'),
-models      = require('./app/models');
+models      = require('./app/models'),
+middlewares = require('./app/middlewares'),
+controllers = require('./app/controllers');
 
 app.http().io();
 
@@ -14,6 +17,7 @@ options.server.socketOptions  = { keepAlive: 1 };
 options.replset.socketOptions = { keepAlive: 1 };
 mongoose.connect(settings.mongo.url,options);
 
+// session
 app.io.session({
 	store:new MongoStore({
 		url:settings.mongo.url
@@ -23,11 +27,30 @@ app.io.session({
 	saveUninitialized: true
 });
 
+// third middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// view engine
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// publich
+app.use('/public', express.static(__dirname + '/public', { maxAge: '364d' }));
+
+// controllers
+_.map(controllers,function(controller){
+	controller.apply({
+		app:app,
+		middlewares:middlewares,
+		models:models,
+		controllers: controllers
+	});
+});
+
 app.get('/',function(req,res,next){
 	req.session.count++;
 	res.send('count:'+req.session.count);
 });
-
-console.dir(models);
 
 app.listen(80);
--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,24 +1,35 @@
 function user(){
-	var app = app;
-	var middlewares = middlewares;
+	var app         = this.app;
+	var middlewares = this.middlewares;
+	var models      = this.models
 
 	// 首页
-	app.get('/',middlewares.reuireLogin,function(req,res){
+	app.get('/', middlewares.requireLogin, function(req,res){
 		res.send('already login');
 	});
 
 	// 登陆页
-	app.get('/login',middlewares.requireLogin.redirect,function(req,res){
+	app.get('/login', middlewares.requireLogin.redirect, function(req,res){
 		res.render('login',{title:'登陆'});
 	});
 
 	// 登陆操作
-	app.post('/login',middlewares.requireLogin.redirect,function(req,res){
-		
+	app.post('/login', middlewares.requireLogin.redirect, function(req,res){
+		res.send('get it');
+		console.dir(req.body);
 	});
 
 	// 注册操作
-	app.post('/register',middlewares.requireLogin.redirect,function(req,res){
-
+	app.post('/register', middlewares.requireLogin.redirect, function(req,res){
+		var user = new models.user(req.body);
+		user.save(function(err){
+			if(err){
+				res.send('注册出错');
+			}else{
+				res.send('注册成功');
+			}
+		});
 	});
 }
+
+module.exports = user;
